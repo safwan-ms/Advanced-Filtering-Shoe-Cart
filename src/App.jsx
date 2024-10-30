414e8615b89cd5b0499dd23242070491e8915da7
safwan-ms <safwan.anasms@gmail.com>
2024-10-30 18:20:08 +0530

    saved changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import Nav from "./Navigation/Nav";
 import Products from "./Products/Products";
 import Recommended from "./Recommended/Recommended";
-import SideBar from "./SideBar/SideBar";
+import SideBar from " ./SideBar/SideBar";
+
+// Data
+import products from "./db/data";
+
+// Input Filter
+const [query, setQuery] = useState("");
+
+const handleInput = (event) => {
+  setQuery(event.target.value);
+};
+
+const filteredItem = products.filter((product) =>
+  product.title.toLocaleLowerCase().indexOf(query.toLocaleLowerCase() !== -1)
+);
+
 const App = () => {
   return (
     <>
